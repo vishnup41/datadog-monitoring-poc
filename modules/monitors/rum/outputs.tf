a02ba97793250fb143ec1644e97517d2420f3e10
vishnup41 <54897000+vishnup41@users.noreply.github.com>
2026-04-01 10:54:28 +0530

    Create outputs.tf to export monitor_ids as a map.

---
+++ modules/monitors/rum/outputs.tf
@@ -0,0 +1,9 @@
+// outputs.tf
+
+output "monitor_ids" {
+  description = "Map of monitor IDs"
+  value = {
+    // Add your monitor IDs here, for example:
+    // "monitor_name" = monitor_id
+  }
+}

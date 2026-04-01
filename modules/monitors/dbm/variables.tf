1aa42e401c214d712fcffdceadf3aa1024a4a1e8
vishnup41 <54897000+vishnup41@users.noreply.github.com>
2026-04-01 11:14:29 +0530

    Create variables.tf for team_name, service_name, and environment

---
+++ modules/monitors/dbm/variables.tf
@@ -0,0 +1,14 @@
+variable "team_name" {
+  description = "The name of the team"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the service"
+  type        = string
+}
+
+variable "environment" {
+  description = "The deployment environment"
+  type        = string
+}

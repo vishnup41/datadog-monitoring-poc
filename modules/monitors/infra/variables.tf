dfe8481c002acad4b37f2b957bf4d019e65a254d
vishnup41 <54897000+vishnup41@users.noreply.github.com>
2026-04-01 10:54:58 +0530

    Add Terraform variables for team, service, and environment

---
+++ modules/monitors/infra/variables.tf
@@ -0,0 +1,14 @@
+variable "team_name" {
+  description = "Name of the team"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Name of the service"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
